1d450ceb22b6fd27d653e72cb6c97b3fc3c896d5
danielavalera <dvaler01@ucm.es>
2023-09-24 13:37:35 +0200

    Subida la resolución de JUEZ/1.3

---
+++ Juez/Resto de ejercicios/1.3 - Primera palabra/source.cpp
@@ -0,0 +1,67 @@
+// Daniela Valentina Valera Fuentes
+// F94
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+// función que resuelve el problema
+//TipoSolucion resolver(TipoDatos datos) {}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+void resuelveCaso() {
+    // leer los datos de la entrada
+    int nPalabras;
+    string palabra;
+    vector<string> p;
+
+    cin >> nPalabras;
+    for (int i = 0; i < nPalabras; i++)
+    {
+        cin >> palabra;
+        p.push_back(palabra);
+    }
+
+    string primeraPalabraEnOrden = p[0];
+
+    for (int i = 0; i < p.size(); i++)
+    {
+        if (p[i] < primeraPalabraEnOrden) {
+            primeraPalabraEnOrden = p[i];
+        }
+    }
+
+    //TipoSolucion sol = resolver(datos);
+    // escribir sol
+
+    cout << primeraPalabraEnOrden << endl;
+
+}
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+#ifndef DOMJUDGE
+    std::ifstream in("datos1.3.txt");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+    int numCasos;
+    std::cin >> numCasos;
+    for (int i = 0; i < numCasos; ++i)
+        resuelveCaso();
+
+
+    // Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    std::cin.rdbuf(cinbuf);
+    system("PAUSE");
+#endif
+
+    return 0;
+}
