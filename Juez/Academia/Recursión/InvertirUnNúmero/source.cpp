94e4c1aad4d2a81011a96a2dc47a0d6f0d96590f
danielavalera <dvaler01@ucm.es>
2023-11-06 16:05:26 +0100

    añadida especificación

--- Juez/Academia/Recursión/InvertirUnNúmero/source.cpp
+++ Juez/Academia/Recursión/InvertirUnNúmero/source.cpp
@@ -2,8 +2,38 @@
 // F94
 
 /*
-* COSTE:
+* COSTE: O(n) siendo n el número de cifras
 * 
+* ESPECIFICACIÓN:
+*
+*       tenemos dos casos base 
+*       (no tiene que ser literalmente lo que hay en el código, 
+*       hay que quedarse con la idea del algoritmo)
+*       -Función de recurrencia:
+*               T(n) = c0             si n = 0
+*               T(n) = c1 + T(n-1)    si n > 0
+*                siendo n el nº de cifras
+*
+*                **c0 y c1 son constantes
+*                **T(n-1) xq en cada vuelta se reduce n una vez
+*
+*
+*
+*
+*       -Desplegado:
+*               T(n) = c1 + T(n-1) //de primeras, es copiar esta función de la f.de recurrencia
+*               En la segunda vuelta: T(n) = c1 + T(n-1) * c1 + c1 + T(n-2)
+*               En la tercera... T(n) = c1 + T(n-1) * c1 + c1 + T(n-2) * c1 + c1 + c1 + T(n-3) = 3c1 + T(n-3)
+*               ...
+*               T(n) = k*c1 + T(n-k)
+*
+*
+*       Decir que ese n-k queremos que sea un 0. Lo que nos queda que n = k
+*       -Postulado:
+*               n-k = 0 -> n = k
+*               Por lo que si sustituimos en la función de desplegado (T(n) = k*c1 + T(n-k)):
+*               k*c1 + T(n-k) = nc1 + T(n-n) = nc1 + T(0) = nc1 + c0 = n , por lo que ahora podemos especificar el coste:
+*               
 */
 
 #include <iostream>
